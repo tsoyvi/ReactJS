d37fdfdbe807dd2de4f139dcd541dae7b897a355
tsoyvi <tsoyvi@inbox.ru>
2022-09-26 19:25:54 +0500

    Lesson_2

--- src/App.js
+++ src/App.js
@@ -1,99 +1,90 @@
 import './css/style.css';
 
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
+import MessagesList from './components/MessagesList';
 
 
 function App(props) {
 
-  const [messageList, setMessageList] = useState(
-    [
-      {
-        id: '0',
-        title: 'message_1',
-        text: 'Lorem ipsum dolor sit amet,',
-        author: 'ClassiK',
-        date: '21.09.2022'
-      },
-      {
-        id: '1',
-        title: 'message_2',
-        text: 'But I must explain to you how all this mistaken idea',
-        author: 'Gauss',
-        date: '21.09.2022'
-      },
-      {
-        id: '2',
-        title: 'message_3',
-        text: 'On the other hand, we denounce with righteous indignation',
-        author: 'NoName',
-        date: '21.09.2022'
-      },
-    ]
-  );
+  const [messageList, setMessageList] = useState([]);
 
-  const [state, setState] = useState(0);
-  const [value, setValue] = useState('');
-  const [MessageValue, setMessageValue] = useState('uyuy');
+  const [messageText, setMessageText] = useState('');
+  const [messageAuthor, setMessageAuthor] = useState('');
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    setMessageList(prevState => [...prevState, {
+      id: messageList.length,
+      title: 'message_' + (messageList.length + 1),
+      text: messageText,
+      author: messageAuthor,
+      date: new Date().toLocaleDateString()
+    }]);
+
+    setMessageText('');
+    setMessageAuthor('');
+  }
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      robotAnswer();
+    }, 3000)
+  }, [messageList]);
+
+
+  function robotAnswer() {
+    const lastAuthor = messageList[messageList.length - 1];
+    if (lastAuthor && lastAuthor.author) {
+      setMessageList(prevState => [...prevState, {
+        id: messageList.length,
+        title: 'message_' + (messageList.length + 1),
+        text: 'Сообщение отправлено',
+        date: new Date().toLocaleDateString()
+      }]);
+
+    }
+
+  }
 
   return (
-    <div>
-      <input value={value} onChange={(event) => setValue(event.target.value)} />
-      <hr />
-      <button onClick={() => setState(state - 1)}>-</button>
-      <button onClick={() => setState(state + 1)}>+</button>
-      <h4>{state}</h4>
-      {messageList.map((item) => <div key={item.id}>
-        <div className="alert alert-success" role="alert">
-          <h5 className="alert-heading">{item.title}</h5>
-          <p>{item.text}</p>
-          <hr />
-          <p className="mb-0">{item.author} {item.date}</p>
+
+    <div className="w-50 p-3">
+
+      <MessagesList messList={messageList} />
+
+      <form onSubmit={sendMessage}>
+        <div className="form-group">
+          <label>Сообщение</label>
+          <input type="text" className="form-control" value={messageText} onChange={(event) => setMessageText(event.target.value)} />
         </div>
-      </div>
-      )}
 
-      <div className="form-group">
-        <label for="exampleInputEmail1">Сообщение</label>
-        <input type="text" class="form-control" value={MessageValue} onChange={(event) => setMessageValue(event.target.MessageValue)} />
-      </div>
+        <div className="form-group">
+          <label>Автор</label>
+          <input type="text" className="form-control" value={messageAuthor} onChange={(event) => setMessageAuthor(event.target.value)} />
+        </div>
 
-      <br />
-      <div className="form-group">
-        <button className="btn btn-primary" onClick={addMessage} >
-          Отправить
-        </button>
-      </div>
-
+        <br />
+        <div className="form-group">
+          <button className="btn btn-primary" type='submit' >
+            Отправить
+          </button>
+        </div>
+      </form>
     </div>
   );
 
+  /*
+  
+  */
 
-  function addMessage() {
-    setMessageList([...messageList, {
-      id: messageList.length,
-      title: 'message_' + (messageList.length + 1),
-      text: MessageValue,
-      author: 'NoName',
-      date: new Date().toLocaleDateString()
-    }]);
-  }
+
 
 
 }
 
 
 
-
-/*
-export function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-  <div>
-  <span className="counter">{count}</span>
-  <button className="counter-button">Click!</button>
-  </div>
-  )
-  }
-*/
 
 export default App;
